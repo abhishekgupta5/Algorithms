953ee15e40aa6b04429fdd05cbf0e558fbb4c5e1
Abhishek Gupta <abhishekmodi2011@gmail.com>
2017-08-23 18:48:39 +0530

    Bug fix in factors

--- factors.py
+++ factors.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-from math import sqrt, ceil
 
 def factors(n):
-    root = ceil(sqrt(n))
+    if n==1:
+        print(1)
+        return
+    root = int((n**(0.5)+1))
     lis=[]
     for i in range(1,root):
         if (n%i == 0):
-            lis.append(int(i))
-            if (i != root):
+            lis.append(i)
+            if (i != n**0.5):
                 lis.append(int(n/i))
+    #lis.sort()
     lis.sort()
     print(lis)
 
